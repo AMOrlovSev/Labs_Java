fcbe41f5c8fd0a4a26840b310f1cb9a1df58462c
Artem Orlov <amorlov.sev@gmail.com>
2024-04-12 12:34:27 +0300

    Labs1,2

---
+++ src/lab2/Calculator.java
@@ -0,0 +1,68 @@
+package lab2;
+
+public class Calculator {
+
+    //Sum
+    public double Sum(double number1, double number2) {
+        double result = number1 + number2;
+        return result;
+    }
+
+    public long Sum(long number1, long number2) {
+        long result = number1 + number2;
+        return result;
+    }
+
+    public int Sum(int number1, int number2) {
+        int result = number1 + number2;
+        return result;
+    }
+
+    //Division
+    public double Division(double number1, double number2) {
+        double result = number1 / number2;
+        return result;
+    }
+
+    public long Division(long number1, long number2) {
+        long result = number1 / number2;
+        return result;
+    }
+
+    public int Division(int number1, int number2) {
+        int result = number1 / number2;
+        return result;
+    }
+
+    //Multiplication
+    public double Multiplication(double number1, double number2) {
+        double result = number1 * number2;
+        return result;
+    }
+
+    public long Multiplication(long number1, long number2) {
+        long result = number1 * number2;
+        return result;
+    }
+
+    public int Multiplication(int number1, int number2) {
+        int result = number1 * number2;
+        return result;
+    }
+
+    //Subtraction
+    public double Subtraction(double number1, double number2) {
+        double result = number1 - number2;
+        return result;
+    }
+
+    public long Subtraction(long number1, long number2) {
+        long result = number1 - number2;
+        return result;
+    }
+
+    public int Subtraction(int number1, int number2) {
+        int result = number1 - number2;
+        return result;
+    }
+}
